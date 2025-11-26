e3f92bae29e28a7fe40d5a96ce581bf3307adba2
SoraYaki <pakli8104@gmail.com>
2025-11-26 07:48:56 +0700

    Add files via upload

--- frm_PopupDetailPendaftaran.pas
+++ frm_PopupDetailPendaftaran.pas
@@ -10,7 +10,7 @@ uses
   cxDBData, cxContainer, Vcl.Menus, Vcl.StdCtrls, cxButtons, cxLabel,
   Vcl.ExtCtrls, cxGridLevel, cxClasses, cxGridCustomView, cxGridCustomTableView,
   cxGridTableView, cxGridDBTableView, cxGrid, dm_desktopPklMagang, cxTextEdit,
-  dxBarBuiltInMenu, cxPC;
+  dxBarBuiltInMenu, cxPC, cxImage, dxGDIPlusClasses, SharedFunctions;
 
 type
   TfrmPopupDetailPendaftaran = class(TForm)
@@ -35,11 +35,86 @@ type
     v_Siswaid_pembimbing_i: TcxGridDBColumn;
     cxStyleRepository: TcxStyleRepository;
     stl_myBold: TcxStyle;
-    cxPageControl1: TcxPageControl;
-    cxTabSheet1: TcxTabSheet;
-    cxTabSheet2: TcxTabSheet;
+    pcMain: TcxPageControl;
+    tabList: TcxTabSheet;
+    tabDetail: TcxTabSheet;
+    labelDataDiri: TcxLabel;
+    panelContainer: TPanel;
+    panelSide: TPanel;
+    imageSiswa: TcxImage;
+    panelData: TPanel;
+    pane01lNama: TPanel;
+    dataNama: TcxTextEdit;
+    labelNama: TcxLabel;
+    panel02NISN: TPanel;
+    dataNISN: TcxTextEdit;
+    labelNISN: TcxLabel;
+    panel03kelamin: TPanel;
+    dataKelamin: TcxTextEdit;
+    labelKelamin: TcxLabel;
+    panel04TempatLahir: TPanel;
+    dataTempatLahir: TcxTextEdit;
+    labelTempatLahir: TcxLabel;
+    panel05TanggalLahir: TPanel;
+    dataTanggalLahir: TcxTextEdit;
+    labelTanggalLahir: TcxLabel;
+    panel06Jurusan: TPanel;
+    dataJurusan: TcxTextEdit;
+    labelJurusan: TcxLabel;
+    panel07AsalSekolah: TPanel;
+    dataSekolah: TcxTextEdit;
+    labelSekolah: TcxLabel;
+    panel08NoHp: TPanel;
+    dataNoHp: TcxTextEdit;
+    labelNoHp: TcxLabel;
+    panel09Email: TPanel;
+    dataEmail: TcxTextEdit;
+    labelEmail: TcxLabel;
+    panel10Alamat: TPanel;
+    dataAlamat: TcxTextEdit;
+    labelAlamat: TcxLabel;
+    labelDataPkl: TcxLabel;
+    panel11Tujuan: TPanel;
+    dataTujuan: TcxTextEdit;
+    labelTujuan: TcxLabel;
+    panel12Progli: TPanel;
+    dataProgli: TcxTextEdit;
+    labelProgli: TcxLabel;
+    panel13MulaiPkl: TPanel;
+    dataTglMulai: TcxTextEdit;
+    labelTglMulai: TcxLabel;
+    panel14SelesaiPkl: TPanel;
+    dataTgLSelesai: TcxTextEdit;
+    labelTglSelesai: TcxLabel;
+    panel15DurasiPkl: TPanel;
+    dataDurasi: TcxTextEdit;
+    labelDurasi: TcxLabel;
+    labelBerkas: TcxLabel;
+    panel16Surat: TPanel;
+    labelSurat: TcxLabel;
+    btnSurat: TcxButton;
+    panel17PasFoto: TPanel;
+    labelPasFoto: TcxLabel;
+    btnFoto: TcxButton;
+    labelAksi: TcxLabel;
+    btnTerima: TcxButton;
+    containerBtnTerima: TPanel;
+    containerBtnTolak: TPanel;
+    bntTolak: TcxButton;
+    containerBtnKembali: TPanel;
+    btnKembali: TcxButton;
+    panel16Pembimbing: TPanel;
+    labelPembimbing: TcxLabel;
+    panelPembimbing: TPanel;
+    panelNamaP: TPanel;
+    dataNamaP: TcxTextEdit;
+    labelNamaP: TcxLabel;
+    panelKontakP: TPanel;
+    dataKontakP: TcxTextEdit;
+    labelKontakP: TcxLabel;
     procedure btnCloseClick(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure ContainerRounded(Sender: TObject);
   private
     { Private declarations }
   public
@@ -52,10 +127,17 @@ implementation
 
 {$R *.dfm}
 
+procedure TfrmPopupDetailPendaftaran.ContainerRounded(Sender: TObject);
+begin
+  SetRoundedBorder(Sender as TWinControl, 17);
+end;
+
+
 procedure TfrmPopupDetailPendaftaran.btnCloseClick(Sender: TObject);
 begin
   ModalResult := mrClose;
 end;
+
 
 procedure TfrmPopupDetailPendaftaran.FormClose(Sender: TObject;
   var Action: TCloseAction);
